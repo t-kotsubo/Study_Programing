87605e843e93a672cc3f07e200a4c67b010504f0
Takayuki.Kotsubo <Takayuki.Kotsubo@logixsquare.com>
2019-03-15 16:28:19 +0900

    追加：20190315

---
+++ Python学習/datetime_test.py
@@ -0,0 +1,11 @@
+import datetime
+
+# today = datetime.date.today()
+# print(today)
+
+today = datetime.date.today()
+print(today)
+
+tomorrow = today + datetime.timedelta(days=1)
+print(tomorrow)
+
