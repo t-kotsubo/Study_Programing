46c4b5a38bbbaac311539d7d569c9c203297bf68
Takayuki.Kotsubo <Takayuki.Kotsubo@logixsquare.com>
2019-07-25 14:45:33 +0900

    Python学習更新_20190725

--- Python学習/for.py
+++ Python学習/for.py
@@ -1,19 +1,22 @@
-# list = []
-# list.append("A")
-# list.append("B")
-# list.append("F")
-# list.append("O")
+# リストの宣言及び要素の追加
+list = []
+list.append("A")
+list.append("B")
+list.append("F")
+list.append("O")
 
-# for i in list:
-#     print(i)
+for i in list:
+    print(i)
 
-# test_list_1 = [['https', 'www'], ['python-izm', 'com']]
+# 2次元配列
+test_list_1 = [['https', 'www'], ['python-izm', 'com']]
  
-# for value in test_list_1:
-#     print(value)
+for value in test_list_1:
+    print(value)
  
-# for value_1, value_2 in test_list_1:
-#     print(value_1, value_2)
+ # リストからループで複数の要素を取り出して処理する場合
+for value_1, value_2 in test_list_1:
+    print(value_1, value_2)
 
 # for i in range(30):
 #     if i%2==0:
@@ -22,5 +25,3 @@
 #     if i==13:
 #         break
 
-for i in range(3,10+1):
-    print(i)
